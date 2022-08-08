ad3efd5eacbc766d17a03a22954d9babf65b101d
Laveytex <YanAlex13@mail.ru>
2022-08-08 16:45:04 +0400

    Com_03

--- Source/MY_GeometrySandbox/MY_GeometrySandboxGameModeBase.cpp
+++ Source/MY_GeometrySandbox/MY_GeometrySandboxGameModeBase.cpp
@@ -2,4 +2,11 @@
 
 
 #include "MY_GeometrySandboxGameModeBase.h"
+#include "SandboxPawn.h"
+#include "SandboxPlayerController.h"
 
+AMY_GeometrySandboxGameModeBase::AMY_GeometrySandboxGameModeBase()
+{
+	DefaultPawnClass = ASandboxPawn::StaticClass();
+	PlayerControllerClass = ASandboxPlayerController::StaticClass();
+}
